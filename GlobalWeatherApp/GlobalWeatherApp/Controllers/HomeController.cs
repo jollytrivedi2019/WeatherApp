360e2daa98c4bb644fa8c6316a05399731bd1a86
jollytrivedi2019 <58602491+jollytrivedi2019@users.noreply.github.com>
2019-12-06 22:00:26 +0530

    Add files via upload

---
+++ GlobalWeatherApp/GlobalWeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GlobalWeatherApp.Models;
+using Newtonsoft.Json;
+
+namespace GlobalWeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Weather()
+        {
+            ViewData["Message"] = "";
+
+            return View();
+        }
+
+        public JsonResult GetWeather()
+        {
+            Weather weatherinfo = new Weather();
+            return Json(weatherinfo.getWeatherForcast());
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GlobalWeatherApp/GlobalWeatherApp/Models/Weather.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GlobalWeatherApp.Models
+{
+    public class Weather
+    {
+        public Object getWeatherForcast()
+        {
+            List<WeatherDetails> lstWeather = new List<WeatherDetails>();
+            string appid = "aa69195559bd4f88d79f9aadeb77a8f6";
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=" + appid + "&units=imperial";
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            string strTemp, strHumidity;
+            //loop to populate city name
+            foreach (CityInfo city in Enum.GetValues(typeof(CityInfo))) {
+
+                string url1 = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + appid + "&units=imperial";                
+                client = new WebClient();
+                content = client.DownloadString(url1);
+                strTemp = content.ToString().Substring(content.ToString().IndexOf("temp") + 6, 5);
+                strHumidity = content.ToString().Substring(content.ToString().IndexOf("humidity") + 10, 2);
+                lstWeather.Add(new WeatherDetails(city.ToString(), strTemp, strHumidity));
+
+            }
+
+            //var jsonContent = JsonConvert.DeserializeObject(content);
+            
+            using(StreamWriter sw = File.CreateText("list.csv"))
+{
+                for (int i = 0; i < lstWeather.Count; i++)
+                {
+                    sw.WriteLine("City : " + lstWeather[i].City);
+                    sw.WriteLine("Humidity : " + lstWeather[i].Humidity);
+                    sw.WriteLine("Temperature : " + lstWeather[i].Temperature);
+                }
+            }
+            //return class as list
+            return lstWeather;
+        }
+        enum CityInfo
+        {            
+            Paris,
+            Dublin,
+            Washington,            
+            Delhi,
+            Mumbai,
+            Rome,
+            Berlin,
+            Dubai,
+            London
+        }
+        public class WeatherDetails
+        {
+            public string City { get; set; }
+            public string Temperature{ get; set; }
+
+            public string Humidity { get; set; }
+
+            public WeatherDetails(string city, string temp, string hum)
+            {
+                City = city;
+                Temperature = temp;
+                Humidity = hum;
+
+            }
+        }
+    }
+}
